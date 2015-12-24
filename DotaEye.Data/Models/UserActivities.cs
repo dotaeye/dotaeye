4356ad8375e867c77209bc443c068d7399e5b228
navy235 <shenhaijunmail@163.com>
2015-12-24 18:20:21 +0800

    add data model

---
+++ DotaEye.Data/Models/UserActivities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using SQ.Core.Data;
+
+namespace DotaEye.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using SQ.Core.Data;
+
+    public partial class UserActivities : BaseEntity
+    {
+        public System.DateTime Time { get; set; }
+        public int UserId { get; set; }
+        public UserActivityType Type { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ DotaEye.Data/Models/UserActivityType.cs
@@ -0,0 +1,10 @@
+﻿namespace DotaEye.Data.Models
+{
+    using System;
+
+    public enum UserActivityType : int
+    {
+        Login = 1,
+        Suggest = 2
+    }
+}
